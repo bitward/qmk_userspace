b9c62c41505b05dce757d874b12a015fb9bf0fa3
Jay Lawrence <bitward@users.noreply.github.com>
2025-01-21 00:29:56 -0600

    got the layout working

---
+++ keyboards/boardsource/unicorne/keymaps/initial/rules.mk
@@ -0,0 +1,11 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+BOOTLOADER = rp2040
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+JOYSTICK_ENABLE = no
+POINTING_DEVICE_ENABLE = no
+#ORYX_ENABLE = yes
+#RGB_MATRIX_CUSTOM_KB = yes
+#TAP_DANCE_ENABLE = yes
+SPACE_CADET_ENABLE = no
